5bfdb4b2f7c5768a472dc9abd49dc535c4648374
fasiondog <fasiondog@163.com>
2024-07-08 03:10:20 +0800

    update

--- hikyuu/utilities/string_view.h
+++ hikyuu/utilities/string_view.h
@@ -1,8 +1,8 @@
 /*
- *  Copyright (c) 2019~2021, Yihua, All rights reserved.
+ *  Copyright (c) 2019~2021, hikyuu
  *
  *  Created on: 2021/12/16
- *      Author: linjinhai
+ *      Author: fasiondog
  */
 
 #pragma once
