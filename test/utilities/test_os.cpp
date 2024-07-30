b11ff9613e61724fba9dbdcca5b5c17de136c192
fasiondog <fasiondog@163.com>
2024-07-30 13:29:33 +0800

    update

--- test/utilities/test_os.cpp
+++ test/utilities/test_os.cpp
@@ -13,7 +13,6 @@
 
 using namespace hku;
 
-#if 0
 static void createTestFile(const std::string &filename) {
     FILE *fp = fopen(HKU_PATH(filename).c_str(), "wb");
     int len = 10;
@@ -160,4 +159,3 @@ TEST_CASE("test_getDiskFreeSpace") {
     HKU_INFO("disk free space /: {}", getDiskFreeSpace("/"));
     HKU_INFO("disk free space .: {}", getDiskFreeSpace("."));
 }
-#endif
